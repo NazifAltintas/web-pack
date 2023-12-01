c9626adf08969a27931e67a38bc80a8724922512
LanternCode <nazifltnts@gmail.com>
2023-12-01 23:52:05 +0100

    dec01

---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+const prod = {
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.[contenthash].js",
+    clean: true,
+  },
+};
+
+module.exports = merge(common, prod);
