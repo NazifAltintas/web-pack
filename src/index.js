112e1cf1ab28c95639837ca0f69ccdada4fd725c
LanternCode <nazifltnts@gmail.com>
2023-12-02 23:34:44 +0100

    dec02

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import "./index.scss";
 
 document.getElementById("btnSayHello").addEventListener("click", () => {
     alert("Hello Webpack....");
+    const arr=[50,60,70,80,85,90,95];
+    console.log(getItem([...arr]));
+    console.log(arr);
+    
     setItem("token", "96634-544444444444444444");
 });
+
+const getItem=(arr)=>{
+    return arr.filter((item)=> item>75).reduce((acc,curr)=> acc+curr,1000)
+}
 
 
